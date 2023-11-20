ec6dd83cef01fd165d14737ca1366f5a7a71dcd7
ffucile <francesco.fucile@prolific.co>
2023-11-20 14:41:58 +0000

    update logic

--- src/main/roverManager.ts
+++ src/main/roverManager.ts
@@ -1,20 +1,23 @@
 export class RoverManager {
+  
+  const directions = ["N", "E", "S", "W"];
+  let currentDirectionIndex = 0;
+
   Execute(command: string) {
     const commands = command.split('');
-    const directions = ["N", "E", "S", "W"];
-    let currentDirection = "N";
-    commands.forEach((c)=> {
-      currentDirection = directions[]
+    commands.forEach((c) => {
+      if (c === 'R') {
+        currentDirectionIndex = (currentDirectionIndex + 1) % 4
+      }
+      if (c === 'M') {
+        return "0:0:N"
+      }
     })
-    if (command === "R") {
-      return "0:0:E";
-    }
-    if (command === "RR") {
-      return "0:0:S";
-    }
-    if (command === "RRR") {
-      return "0:0:W";
-    }
-    return "0:0:N";
+    return
+  }
+
+
+  private AssemblePosition() {
+
   }
 }
